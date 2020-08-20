3895c86fca34f07a6a0b58d447942f4c44dad70c
Tao Li <taolicd@gmail.com>
2020-08-19 17:44:37 -0700

    Add files via upload

---
+++ Search_Engine_python.py
@@ -0,0 +1,29 @@
+﻿from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+import os
+os.chdir('c:\\ELK\\')
+app = Flask(__name__)
+es = Elasticsearch('127.0.0.1', port=9200)
+
+if __name__ == '_main__':
+ app.run('127.0.0.1', debug=True)
+
+@app.route('/')
+def home():
+ return render_template('search.html')
+
+@app.route('/search/results', methods=['GET','POST'])
+def request_search():
+ search_term = request.form["input"]
+ res = es.search(
+ index='test',
+ body={
+ "query" : {"match": {"content": search_term}},
+ "highlight" : {"pre_tags" : ["<b>"] , "post_tags" : ["</b>"], "fields" : {"content":{}}}})
+ res['ST']=search_term
+
+ for hit in res['hits']['hits']:
+     hit['good_summary']='….'.join(hit['highlight']['content'][1:])
+     return render_template('results.html', res=res)
+
+
